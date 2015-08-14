414edd56f3dd04d6931146dfa82474e35f1269ed
wrn <615331179@qq.com>
2015-08-14 17:36:03 +0800

    机票订单

--- js/Controller/TravelConController.js
+++ js/Controller/TravelConController.js
@@ -7,5 +7,9 @@ angular.module('appTravel' , []).controller('TravelConController' , function($sc
     var param = lyf.getUrlPram();
     var server = new ClientServer($http , $scope);
     var funName = param.type == 'hotelCon' ? 'getHotelDetal' : 'getTravelDetal';
-        server.createRequest(param.apiName , funName+'/id/'+param.id , param.templateName);
+        server.createRequest(param.apiName , funName+'/id/'+param.id , '').then(function(d){
+            $scope.travelDetal = d;
+            $(".tra_block").html(d.li.productinfo)
+        })
+
 })
